275c8863b52db9f18f1f8a1c0c8c2c6169503a6d
Hector Camilo Torres Diaz <hcamilotorresdiaz@gmail.com>
2023-05-10 21:18:43 -0500

    Se agrego gano maquina

--- Piedra_Papel_Tijera.py
+++ Piedra_Papel_Tijera.py
@@ -1,31 +1,31 @@
 import random
 
 options = ('piedra','papel','tijera')
+
+user_option = input('piedra, papel o tijera => ')
+user_option = user_option.lower()
+
+if not user_option in options:
+    print('esa opcion es invalida')
+
 computer_option = random.choice(options)
-user_option= 1
-while user_option not in options:
-user_option = input("piedra, papel o tijera: ").lower()
+
+print('User option => ', user_option)
+print('}computer option => ', computer_option)
 
 if user_option == computer_option:
-print(“Empate!”)
-elif user_option == “piedra”:
-if computer_option == “tijera”:
-print(“piedra gana a tijera”)
-print(“user gano!”)
+    print('Empate')
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('Piedra gana a tijera')
+        print('Usuario le gano a la maquina')
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print('Papel gana a la tijera')
+        print('Usuario le gano a la maquina')
+elif user_option == 'tijera':
+    if computer_option == 'papel':
+        print('Tijera le gana al papel')
+        print('Usuario le gano a la maquina')
 else:
-print(“Papel gana a piedra”)
-print(“computer gano!”)
-elif user_option == “papel” :
-if computer_option == “piedra”:
-print(“papel gana a piedra”)
-print(“user gano!”)
-else:
-print(“tijera gana a papel”)
-print(“computer gano!”)
-else: # user tiene tijera
-if computer_option == “papel”:
-print(“tijera gana a papel”)
-print(“user gano!”)
-else:
-print(“piedra gana a tijera”)
-print(“computer gano!”)
+    print('Gano maquina')
